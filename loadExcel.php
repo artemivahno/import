2e6fc420403bf1fabd4abdf821a57ac6b0870e70
artemama <artsiom.ivahno@gmail.com>
2019-05-04 14:55:14 +0300

    1

--- loadExcel.php
+++ loadExcel.php
@@ -1,1 +1,14 @@
 <?php
+var_dump($_FILES);
+            $fname = $_FILES['file']["tmp_name"];
+            echo 'NAME: ' . $fname;
+            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($fname);
+            $sheet = $spreadsheet->getActiveSheet();
+            $rows = $sheet->toArray();
+            foreach ($rows AS $r) {
+                echo '-----------------------------------------------<br/>';
+                foreach ($r AS $c) {
+                    echo '[' . $c . ']';
+                }
+                echo '<br/>';
+            }
