9ea597a1d6af019fe7077bf010abfa4e60118648
artsiom.ivahno@gmail.com <7880707artem>
2019-05-15 21:24:18 +0300

    fill merge cell - ok

--- loadExcel.php
+++ loadExcel.php
@@ -2,31 +2,9 @@
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
-
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 require_once 'vendor/autoload.php';
-//include_once 'imgFromExcel.php';
-
-//Для загрузки файла на сервер, на данный момент не используем
-/*@mkdir("files", 0777);
-$uploaddir = './files/';
-$uploadfile = $uploaddir . basename($_FILES['uploadfile']['name']);
-
-// Копируем файл из каталога для временного хранения файлов:
-if(copy($_FILES['uploadfile']['tmp_name'], $uploadfile)) {
-	echo "<h3>Файл успешно загружен на сервер</h3>";
-}
-else {
-	echo "<h3>Ошибка! Не удалось загрузить файл на сервер!</h3>";
-}
-
-// Выводим информацию о загруженном файле:
-echo "<h3>Информация о загруженном на сервер файле: </h3>";
-echo "<p><b>Оригинальное имя загруженного файла: " . $_FILES['uploadfile']['name'] . "</b></p>";
-echo "<p><b>Размер загруженного файла в байтах: " . $_FILES['uploadfile']['size'] . "</b></p>";
-echo "<p><b>Временное имя файла: " . $_FILES['uploadfile']['tmp_name'] . "</b></p>";*/
-
 
 $inputFileName = $_FILES['uploadfile']["tmp_name"];
 echo 'TMP-FILE-NAME: ' . $inputFileName;
@@ -47,8 +25,6 @@ foreach ($loadedSheetNames as $sheetIndex => $loadedSheetName) {
 
     echo "========================++++++++++++++++++++++++========================================" . '<br/>'
         . "Номер и имя листа: " . ($sheetIndex . ' -> ' . $loadedSheetName) . '<br/>';
-
-
 
 
     //printArrayAsTable($worksheetArray);
@@ -97,53 +73,34 @@ foreach ($loadedSheetNames as $sheetIndex => $loadedSheetName) {
         }
         die("sdfgsdf");
         echo "ALL IMAGES ARE SAVED";*/
-
-
 //======================================
-//-----------------
 
-
-//Заполнияем MergeCells
+    //Заполнияем MergeCells
     $mergedCellsRange = $worksheet->getMergeCells();
     print_r($mergedCellsRange);
 
     foreach ($mergedCellsRange as $currMergedRange) {
 
-        echo "<br>";
-        print_r($currMergedRange);
-        echo "<br>";
-
+        //A1,B1.C1
+        $columnIndex = Coordinate::extractAllCellReferencesInRange($currMergedRange);
+        echo "<br>" . "columnIndex";
+        print_r($columnIndex);
         $currMergedCellsArray = Coordinate::splitRange($currMergedRange);
-        //print_r($currMergedCellsArray);
+
+
         $cellAdres = $currMergedCellsArray[0][0];
         $cell = $worksheet->getCell($cellAdres)->getValue();
         //print_r($cell);
 
-        foreach ($currMergedCellsArray[0] as $adres){
-            $worksheet->setCellValue($adres,$cell);
-
+        $currMergedRangeAr = $worksheet->rangeToArray($currMergedRange, $cell);
+        //print_r($currMergedRangeAr); //A1:O1
+        foreach ($columnIndex as $adres) {
+            $worksheet->setCellValue($adres, $cell);
         }
-
     }
-
     $worksheetArray = $worksheet->toArray();
-
     printArrayAsTable($worksheetArray);
 
-
-//--end Mergin cells
-
-    /*echo "<table border=\"1\">";
-    foreach ($rows AS $row) {
-
-        echo "<tr>";
-        foreach ($row AS $cell) {
-            echo "<td>" . $cell . "</td>";
-        }
-
-    }
-    echo '<br/>';
-    echo "</table>";*/
 }
 
 
